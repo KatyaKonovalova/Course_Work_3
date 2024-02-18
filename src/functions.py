a60418cd64110b87b7c8f5798c545b33df030789
konov <konovalova_ekaterina_a@mail.ru>
2024-02-18 21:08:50 +0300

    add function getting_description, uotput date, description

--- src/functions.py
+++ src/functions.py
@@ -5,6 +5,8 @@ from datetime import datetime
 list_of_completed_operations = []
 list_path_from = []
 list_path_to = []
+list_date = []
+list_description = []
 
 with open(os.path.join('operations.json'), encoding='utf8') as file:
     all_operations = json.load(file)
@@ -28,6 +30,20 @@ def getting_path_from():
 
         if path_from is None:
             path_from = 'Information is absent'
+            list_path_from.append(path_from)
+
+        elif 'Счет' in path_from:
+            card_number = path_from.split()[-1]
+            formatted_number = '**' + card_number[-4:]
+            path_from = path_from.split()[0] + ' ' + formatted_number
+            list_path_from.append(path_from)
+
+        elif 'Visa' in path_from:
+            card_number = path_from.split()[-1]
+            formatted_number_from = str(
+                card_number[:6] + (len(card_number[6:-4]) * '*') + card_number[-4:])
+            formatted_number_from = '-'.join(formatted_number_from[i * 4:(i + 1) * 4] for i in range(4))
+            path_from = path_from.split()[0] + ' ' + path_from.split()[1] + ' ' + formatted_number_from
             list_path_from.append(path_from)
         else:
             card_number = path_from.split()[-1]
@@ -58,4 +74,32 @@ def getting_path_to():
     return list_path_to
 
 # getting_path_to()
+
+
+def formatted_date():
+    for current_operation in result:
+        change_date = current_operation.get('date')
+        datetime_obj = datetime.strptime(change_date, '%Y-%m-%dT%H:%M:%S.%f')
+        date_result = datetime_obj.strftime("%d.%m.%Y")
+        list_date.append(date_result)
+
+    # print(list_date)
+
+    return list_date
+
+# formatted_date()
+
+
+def getting_description():
+
+    for current_operation in result:
+        description = current_operation.get('description')
+        list_description.append(description)
+
+    # print(list_description)
+
+    return list_description
+
+# getting_description()
+
 
--- src/main_code.py
+++ src/main_code.py
@@ -1,8 +1,15 @@
-from src import utils
-dict_path = {}
+from src import functions
 
-for item in range(len(utils.getting_path_from())):
-    item_for = utils.getting_path_from()[item]
-    item_to = utils.getting_path_to()[item]
+for item in range(len(functions.getting_path_from())):
+    item_for = functions.getting_path_from()[item]
+    item_to = functions.getting_path_to()[item]
+
+for item_day in range(len(functions.formatted_date())):
+    item_date = functions.formatted_date()[item_day]
+
+for item_description in range(len(functions.getting_description())):
+    item_d = functions.getting_description()[item_description]
+    print(item_date, item_d)
     print(f'{item_for} -> {item_to}')
+
 
