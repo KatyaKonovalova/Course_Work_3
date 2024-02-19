542325514d303ec8f01087d8df7808f383fa3277
konov <konovalova_ekaterina_a@mail.ru>
2024-02-19 19:24:20 +0300

    code for operations.json moved to main_code and created new function getting_sorted_operation for it

--- src/functions.py
+++ src/functions.py
@@ -13,24 +13,27 @@ list_operation_currency = []
 list_operation_name = []
 
 
-with open(os.path.join('operations.json'), encoding='utf8') as file:
-    all_operations = json.load(file)
+# with open(os.path.join('../src/operations.json'), encoding='utf8') as file:
+#     all_operations = json.load(file)
+#
+#
+# '''Выборка списка словарей по значению EXECUTED, и из них вывод последних 5 операций'''
+#
+# for operation in all_operations:
+#     for key, value in operation.items():
+#         if key == 'state' and value == 'EXECUTED':
+#             list_of_completed_operations.append(operation)
+#
+# sorted_operations = sorted(list_of_completed_operations, key=operator.itemgetter('date'), reverse=True)
+# result = sorted_operations[:5]
+# print(result)
 
+def getting_path_from(data):
+    """Вывод значений номеров карт/счетов откуда совершалась операция, в формате XXXX-XX**-****-XXXX"""
 
-for operation in all_operations:
-    for key, value in operation.items():
-        if key == 'state' and value == 'EXECUTED':
-            list_of_completed_operations.append(operation)
-
-sorted_operations = sorted(list_of_completed_operations, key=operator.itemgetter('date'), reverse=True)
-result = sorted_operations[:5]
-
-
-def getting_path_from():
-
-    for current_operation in result:
+    for current_operation in data:
         path_from = current_operation.get('from')
-
+        # print(path_from)
         if path_from is None:
             path_from = 'Information is absent'
             list_path_from.append(path_from)
@@ -55,13 +58,14 @@ def getting_path_from():
             formatted_number_from = '-'.join(formatted_number_from[i * 4:(i + 1) * 4] for i in range(4))
             path_from = path_from.split()[0] + ' ' + formatted_number_from
             list_path_from.append(path_from)
-
+        # print(list_path_from)
     return list_path_from
 
 
-def getting_path_to():
+def getting_path_to(data):
+    """Вывод номеров счетов куда совершалась операция, в формате **XXXX"""
 
-    for current_operation in result:
+    for current_operation in data:
         path_to = current_operation.get('to')
         card_number = path_to.split()[-1]
         formatted_number = '**' + card_number[-4:]
@@ -71,8 +75,10 @@ def getting_path_to():
     return list_path_to
 
 
-def formatted_date():
-    for current_operation in result:
+def formatted_date(data):
+    """Вывод даты в формате день.месяц.год"""
+
+    for current_operation in data:
         change_date = current_operation.get('date')
         datetime_obj = datetime.strptime(change_date, '%Y-%m-%dT%H:%M:%S.%f')
         date_result = datetime_obj.strftime("%d.%m.%Y")
@@ -81,18 +87,20 @@ def formatted_date():
     return list_date
 
 
-def getting_description():
+def getting_description(data):
+    """Вывод описания операции"""
 
-    for current_operation in result:
+    for current_operation in data:
         # description = current_operation.get('description')
         list_description.append(current_operation.get('description'))
 
     return list_description
 
 
-def getting_operation_amount():
+def getting_operation_amount(data):
+    """Вывод суммы операции"""
 
-    for current_operation in result:
+    for current_operation in data:
         operation_amount = current_operation.get('operationAmount')
         list_operation.append(operation_amount)
         list_operation_amount.append(operation_amount.get('amount'))
@@ -100,8 +108,10 @@ def getting_operation_amount():
     return list_operation_amount
 
 
-def getting_operation_currency():
-    for current_operation in result:
+def getting_operation_currency(data):
+    """Вывод валюты операции"""
+
+    for current_operation in data:
         operation_amount = current_operation.get('operationAmount')
         list_operation.append(operation_amount)
         list_operation_currency.append(operation_amount.get('currency'))
--- src/main_code.py
+++ src/main_code.py
@@ -1,15 +1,40 @@
-from src import functions
+import os
+import json
+import operator
+import functions
+
+list_of_completed_operations = []
 i = 0
+
+with open(os.path.join('../src/operations.json'), encoding='utf8') as file:
+    all_operations = json.load(file)
+
+
+def getting_sorted_operations(operations):
+    '''Выборка списка словарей по значению EXECUTED, и из них вывод последних 5 операций'''
+
+    for operation in operations:
+        for key, value in operation.items():
+            if key == 'state' and value == 'EXECUTED':
+                list_of_completed_operations.append(operation)
+
+    sorted_operations = sorted(list_of_completed_operations, key=operator.itemgetter('date'), reverse=True)
+    result = sorted_operations[:5]
+    return result
+
+
+result_of_sorted_operations = getting_sorted_operations(all_operations)
+result_formatted_date = functions.formatted_date(result_of_sorted_operations)
+result_description = functions.getting_description(result_of_sorted_operations)
+result_path_from = functions.getting_path_from(result_of_sorted_operations)
+result_path_to = functions.getting_path_to(result_of_sorted_operations)
+result_operation_amount = functions.getting_operation_amount(result_of_sorted_operations)
+result_operation_currency = functions.getting_operation_currency(result_of_sorted_operations)
 
 while i < 5:
 
-    print(functions.formatted_date()[i], functions.getting_description()[i])
-    print(f'{functions.getting_path_from()[i]} -> {functions.getting_path_to()[i]}')
-    print(functions.getting_operation_amount()[i], functions.getting_operation_currency()[i], end='\n\n')
+    print(result_formatted_date[i], result_description[i])
+    print(f'{result_path_from[i]} -> {result_path_to[i]}')
+    print(result_operation_amount[i], result_operation_currency[i], end='\n\n')
 
     i += 1
-
-
-
-
-
