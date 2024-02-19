3c85cb8536e275ce72396eba86e5f8265f41b263
konov <konovalova_ekaterina_a@mail.ru>
2024-02-19 20:27:34 +0300

    working version

--- test/test_functions.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src import main_code
-from src import functions
-
-
-def test_getting_description():
-
-    assert functions.getting_description(main_code.formatted_operations()) == functions.getting_description(main_code.getting_sorted_operations(main_code.formatted_operations()))
-
