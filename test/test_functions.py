796214d231d27984e3b3382461df1818d5fa9bc9
konov <konovalova_ekaterina_a@mail.ru>
2024-02-21 20:46:06 +0300

    add tests in test_functions.py with 100% coverage

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,9 +1,21 @@
-import pytest
-from src import main_code
-from src import functions
+from src.functions import formatted_date
+from src.functions import getting_path_from
+from src.functions import getting_path_to
 
 
-def test_getting_description():
+def test_formatted_date():
 
-    assert functions.getting_description(main_code.formatted_operations()) == functions.getting_description(main_code.getting_sorted_operations(main_code.formatted_operations()))
+    assert formatted_date('2019-08-26T10:50:58.294041') == '26.08.2019'
 
+
+def test_getting_path_from():
+
+    assert getting_path_from(None) == ''
+    assert getting_path_from('Visa Classic 2842878893689012') == 'Visa Classic 2842-87**-****-9012'
+    assert getting_path_from('Maestro 7810846596785568') == 'Maestro 7810-84**-****-5568'
+    assert getting_path_from('Счет 38611439522855669794') == 'Счет **9794'
+
+
+def test_getting_path_to():
+
+    assert getting_path_to('Счет 90424923579946435907') == 'Счет **5907'
